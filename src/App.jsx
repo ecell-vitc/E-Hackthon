035f387c24430cd25be7f76b6794d464fe5b6d3b
V-Srivatsan <vatsan.programming@gmail.com>
2025-01-12 05:57:40 +0000

    Added all components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Banner from './components/Banner'
+import About from './components/About'
+import Participate from './components/WhoCanParticipate'
+import WhyParticipate from './components/WhyParticipate'
+import Sponsor from './components/SponsorSection'
+import FAQ from './components/FAQ'
+import Contact from './components/ContactSection'
+import Footer from './components/Footer'
 
 const App = () => {
 	return (
 		<>
 			<Banner />
 			<About />
+			<Participate />
+			<WhyParticipate />
+			<Sponsor />
+			<FAQ />
+			<Contact />
+			<Footer />
 		</>
 	)
 }
