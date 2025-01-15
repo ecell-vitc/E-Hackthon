62fbbd150f0ca4c2069f4d509c1607700cda2e63
V-Srivatsan <vatsan.programming@gmail.com>
2025-01-15 07:02:55 +0000

    Enabled Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/main.css'
-import App from './App.jsx'
 import './styles/App.css'
+
+import App from './App.jsx'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<App />
+		<Analytics />
 	</StrictMode>,
 )
